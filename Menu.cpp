bd4ef3a51ab0b4a2ea942a28952e00a4ff9681d5
Laplace <Laplace_yongle_he@163.com>
2023-10-19 23:33:32 +0800

    改了个菜单颜色

--- Menu.cpp
+++ Menu.cpp
@@ -19,7 +19,20 @@
                                        BACKGROUND_RED |                 \
                                            BACKGROUND_INTENSITY)
 
+#define UPKEY 72
+#define DOWNKEY 80
 
+static int color(int c){ //2绿色 3浅蓝色 7白色 8灰色 9蓝色 10绿色 11浅蓝色 12红色 13粉红色 14黄色 15白色 0黑色 1蓝色 4红色 5紫色 6黄色
+    //SetConsoleTextAttribute是API设置控制台窗口字体颜色和背景色的函数
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), c);        //更改文字颜色
+    return c;
+}
+
+static inline int Seed()
+{
+    srand((unsigned)time(nullptr)); //设置随机数种子
+    return rand()%15 + 1; //生成 0-14 之间的随机数
+}
 
 void Gotoxy(short x, short y)
 {
@@ -28,13 +41,10 @@ void Gotoxy(short x, short y)
     SetConsoleCursorPosition(hOut, pos);
 }
 
-
-#define UPKEY 72
-#define DOWNKEY 80
-
 void Menu() //主菜单
 {
 
+    color(14);
     std::cout << ("==--------------------向量测试------------------==\n")
      << ("0) 退出程序                                      |\n")
      << ("1) 输出测试                                      |\n")
@@ -52,34 +62,65 @@ void Menu() //主菜单
 
 void CursorPos1(int x) //光标覆盖菜单
 {
+    /*
+     测试函数之前都使用一次GotoXY()调整输出位置
+     */
     switch (x)
     {
         case 0:
+
             std::cout << ("0) 退出程序");
+            /*
+             * 写测试函数
+             */
             break;
         case 1:
             std::cout << ("1) 输出测试");
+            /*
+             * 写测试函数
+             */
             break;
         case 2:
             std::cout << ("2) 向量反转测试");
+            /*
+             * 写测试函数
+             */
             break;
         case 3:
             std::cout << ("3) 迭代器测试");
+            /*
+             * 写测试函数
+             */
             break;
         case 4:
             std::cout << ("4) 越界异常捕获测试");
+            /*
+             * 写测试函数
+             */
             break;
         case 5:
             std::cout << ("5) 插入测试");
+            /*
+             * 写测试函数
+             */
             break;
         case 6:
             std::cout << ("6) 删除测试(erase & clear)");
+            /*
+             * 写测试函数
+             */
             break;
         case 7:
             std::cout << ("7) 追加测试(push_back)");
+            /*
+             * 写测试函数
+             */
             break;
         case 8:
             std::cout << ("8) 弹出测试(pop_back)");
+            /*
+             * 写测试函数
+             */
             break;
     }
 }
@@ -100,7 +141,8 @@ upper, lower：选项的上下界
         if (c2 == DOWNKEY)
         {
             Gotoxy(0, line);
-            IN_DEFAULT; //选择默认色
+//            IN_DEFAULT; //选择默认色
+            color(14);
             (*p)(line - deta); //覆盖，实现退选
             if (line == upper) //如果已经到了上界，则返回下界，实现滚动高亮
                 line = lower;
@@ -113,7 +155,8 @@ upper, lower：选项的上下界
         if (c2 == UPKEY)
         {
             Gotoxy(0, line);
-            IN_DEFAULT;
+//            IN_DEFAULT;
+            color(14);
             (*p)(line - deta);
             if (line == lower)
                 line = upper;
@@ -134,7 +177,7 @@ upper, lower：选项的上下界
 }
 
 void MenuStart() {
-    int n, line = 1; // 对应menu()的1）
+    int n, line = 9; // 对应menu()的1）
     char c1, c2;
     bool b;
     while (1) {
